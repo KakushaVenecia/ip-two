654119fd0d96795504c8763668424232710a7867
KakushaVenecia <kakushavenecia@gmail.com>
2022-03-06 23:28:52 +0300

    changes completed

--- main.js
+++ main.js
@@ -66,10 +66,10 @@ function getGender(){
         alert("You were born on " +dayNames[2]+ " and Your akan name is " +maleNames[2]+"!");
       }
       else if(dayValue == 4){
-        alert("You were born on "+dayNames[3] +  " and Your akan name is " +maleNames[3]+"!");
+        alert("You were born on "+dayNames[3] +  "and Your akan name is " +maleNames[3]+"!");
       }
       else if(dayValue == 5){
-        alert("You were born on "+dayNames[4] +  " and Your akan name is " +maleNames[4]+"!");
+        alert("You were born on "+dayNames[4] +  "and Your akan name is " +maleNames[4]+"!");
       }
       else if(dayValue == 6){
         alert("You were born on "+dayNames[5] +  " and Your akan name is " +maleNames[5]+"!");
