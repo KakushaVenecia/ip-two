cd1f6b0cc88d807a0f40f73b9532c4ba0daf5cce
KakushaVenecia <kakushavenecia@gmail.com>
2022-03-07 05:22:51 +0300

    format complete

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
-var maleNames= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
 
-function generateAkan(){
+function generateAkan() {
   var year = document.getElementById("year").value;
   var month = document.getElementById("month").value;
   var day = document.getElementById("day").value;
-  var gender = document.getElementById ("gender").value;
+  var gender = document.getElementById("gender").value;
   let birthday = new Date(`${year}-${month}-${day}`)
   let dateString = birthday.toDateString()
   let birthDate = birthday.getDay()
-  if (gender == "male"){
+  if (gender == "male") {
     let name = maleNames[birthDate]
-    alert ("You were born on" +  dateString + "Your Akan Name is" + name)
-  }
-  else if (gender == "female"){
+    alert("You were born on" + dateString + "Your Akan Name is" + name)
+  } else if (gender == "female") {
     let name = femaleNames[birthDate]
-    alert ("You were born on" +  dateString + "Your Akan Name is" + name)
-  }
-  else {
+    alert("You were born on" + dateString + "Your Akan Name is" + name)
+  } else {
     alert("Please select gender")
-  }    
-
-  if (month > 12 || month < 1){
-     alert (" Please input a month")
   }
-  if (day > 31 || month <1){
-    alert ("This month inout is invalid")
+
+  if (month > 12 || month < 1) {
+    alert(" Please input a month")
+  }
+  if (day > 31 || month < 1) {
+    alert("This month inout is invalid")
   }
 }
